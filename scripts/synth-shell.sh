ce70be3ef0014003a30946ee3aff7b7806cc069a
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-05 16:10:26 -0400

    Create synth-shell.sh

---
+++ scripts/synth-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install dependencies
+    mkdir $HOME/projects/
+    cd $HOME/projects/
+    git clone --recursive https://github.com/andresgongora/synth-shell.git
+    cd synth-shell/
+    chmod +x setup.sh
+#    cd $HOME/projects/synth-shell
+    echo "i u n Y n n n"| ./setup.sh
