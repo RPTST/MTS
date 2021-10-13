667afa8d0e3affc8d0a34a9b8d1e44a03756a09f
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 18:54:07 -0400

    Create GNU_games.sh

---
+++ scripts/GNU_games.sh
@@ -0,0 +1,13 @@
+# Install GNU flatpack games
+#
+#
+# Install Solitar
+flatpak install flathub org.gnome.Aisleriot
+# Install gnome Chess
+flatpak install flathub org.gnome.Chess
+# Install gnome Mahjongg
+flatpak install flathub org.gnome.Mahjongg
+# Install gnome Sudoku
+flatpak install flathub org.gnome.Sudoku
+# Install gnome Reversi
+flatpak install flathub org.gnome.Reversi
