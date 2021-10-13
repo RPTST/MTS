6962163612fa66fbb31e36f73a512c67848c193f
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 17:11:14 -0400

    Create 0-A_D.sh

---
+++ scripts/0-A_D.sh
@@ -0,0 +1,5 @@
+# Install 0 A.D. (updated)
+sudo apt install flatpak
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo reboot
+flatpak install flathub com.play0ad.zeroad
