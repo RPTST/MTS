2d108df101a4760c770820372c39429f31b1b5c6
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 19:06:28 -0400

    Create Standard_Apps.sh

---
+++ scripts/Standard_Apps.sh
@@ -0,0 +1,66 @@
+# Standard Apps install
+#
+# Productivity
+flatpak install flathub com.abisource.AbiWord
+flatpak install flathub org.libreoffice.LibreOffice
+flatpak install flathub com.simplenote.Simplenote
+#
+# Apps
+flatpak install flathub org.filezillaproject.Filezilla
+flatpak install flathub com.poweriso.PowerISO
+flatpak install flathub org.qbittorrent.qBittorren
+flatpak install flathub com.calibre_ebook.calibre
+flatpak install flathub io.bit3.WhatsAppQT
+flatpak install flathub io.github.peazip.PeaZip
+flatpak install flathub com.bitwarden.desktop
+flatpak install flathub com.github.hluk.copyq
+#
+# Browser
+flatpak install flathub org.gnome.Epiphany
+flatpak install flathub io.gitlab.librewolf-community
+flatpak install flathub com.github.Eloston.UngoogledChromium
+#
+#graphics & videos
+flatpak install flathub io.mpv.Mpv
+flatpak install flathub org.videolan.VLC
+flatpak install flathub com.rawtherapee.RawTherapee
+flatpak install flathub com.hamrick.VueScan
+flatpak install flathub io.github.JaGoLi.ytdl_gui
+flatpak install flathub org.tuxpaint.Tuxpaint
+flatpak install flathub org.flameshot.Flameshot
+#
+# Developer Tools
+flatpak install flathub com.vscodium.codium
+flatpak install flathub io.github.shiftey.Desktop
+flatpak install flathub com.notepadqq.Notepadqq
+flatpak install flathub com.sublimetext.three
+#
+# Install Chat
+flatpak install flathub com.slack.Slack
+flatpak install flathub im.pidgin.Pidgin
+flatpak install flathub io.github.Hexchat
+flatpak install flathub com.microsoft.Teams
+#
+# Install video conference
+flatpak install flathub com.skype.Client
+flatpak install flathub us.zoom.Zoom
+flatpak install flathub org.jitsi.jitsi-meet
+flatpak install flathub com.bluejeans.BlueJeans
+#
+# Install voice conference
+flatpak install flathub info.mumble.Mumble
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.teamspeak.TeamSpeak
+#
+# Install mail
+flatpak install flathub ch.protonmail.protonmail-bridge
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub ch.protonmail.protonmail-import-export-app
+#
+# Install audio
+flatpak install flathub org.ardour.Ardour
+flatpak install flathub org.atheme.audacious
+flatpak install flathub org.clementine_player.Clementine
+flatpak install flathub com.github.wwmm.easyeffects
+flatpak install flathub com.googleplaymusicdesktopplayer.GPMDP
+flatpak install flathub org.gnome.Rhythmbox3
