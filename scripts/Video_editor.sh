159917a7327e54034d18331f71707f8a5e9c4c09
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 19:09:15 -0400

    Create Video_editor.sh

---
+++ scripts/Video_editor.sh
@@ -0,0 +1,55 @@
+# Standard video install
+#
+# Install graphics & videos
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub org.kde.kdenlive
+flatpak install flathub org.shotcut.Shotcut
+flatpak install flathub org.gnome.Cheese
+flatpak install flathub fr.handbrake.ghb
+flatpak install flathub com.makemkv.MakeMKV
+flatpak install flathub io.mpv.Mpv
+flatpak install flathub org.videolan.VLC
+flatpak install flathub com.vinszent.GnomeTwitch
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub org.tuxpaint.Tuxpaint
+flatpak install flathub org.blender.Blender
+flatpak install flathub org.flameshot.Flameshot
+flatpak install flathub org.freecadweb.FreeCAD
+flatpak install flathub org.inkscape.Inkscape
+flatpak install flathub org.kde.krita
+flatpak install flathub net.meshlab.MeshLab
+flatpak install flathub org.openscad.OpenSCAD
+flatpak install flathub com.rawtherapee.RawTherapee
+flatpak install flathub org.gnome.Shotwell
+flatpak install flathub com.hamrick.VueScan
+flatpak install flathub io.github.JaGoLi.ytdl_gui
+#
+# Install Chat
+flatpak install flathub com.slack.Slack
+flatpak install flathub im.pidgin.Pidgin
+flatpak install flathub io.github.Hexchat
+flatpak install flathub com.microsoft.Teams
+#
+# Install video conference
+flatpak install flathub com.skype.Client
+flatpak install flathub us.zoom.Zoom
+flatpak install flathub org.jitsi.jitsi-meet
+flatpak install flathub com.bluejeans.BlueJeans
+#
+# Install voice conference
+flatpak install flathub info.mumble.Mumble
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.teamspeak.TeamSpeak
+#
+# Install mail
+flatpak install flathub ch.protonmail.protonmail-bridge
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub ch.protonmail.protonmail-import-export-app
+#
+# Install audio
+flatpak install flathub org.ardour.Ardour
+flatpak install flathub org.atheme.audacious
+flatpak install flathub org.clementine_player.Clementine
+flatpak install flathub com.github.wwmm.easyeffects
+flatpak install flathub com.googleplaymusicdesktopplayer.GPMDP
+flatpak install flathub org.gnome.Rhythmbox3
