c549ca4ab5fbf3bb4482e859cab58a6432a0b4e2
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 16:47:16 -0400

    Delete openshot.sh

--- openshot.sh
+++
@@ -1,7 +0,0 @@
-# Install openshot
-sudo add-apt-repository ppa:openshot.developers/ppa
-sudo apt install openshot-qt python3-openshot -y
-mkdir -p ~/.openshot_qt/yolo/
-wget -c https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov3.cfg -O ~/.openshot_qt/yolo/yolov3.cfg
-wget -c https://pjreddie.com/media/files/yolov3.weights -O ~/.openshot_qt/yolo/yolov3.weights
-wget -c https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names -O ~/.openshot_qt/yolo/obj.names
