9ceaae2684c20676ea56c8baab24892d0d7f75b6
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 18:57:00 -0400

    Create Game_emulator.sh

---
+++ scripts/Game_emulator.sh
@@ -0,0 +1,30 @@
+# Standard Game emulators install
+#
+# RetroArch enables you to run classic games on a wide range of computers and consoles
+flatpak install flathub org.libretro.RetroArch
+# Playstation 2 'emulator
+flatpak install flathub net.pcsx2.PCSX2
+# PSP emulator
+flatpak install flathub org.ppsspp.PPSSPP
+# Install DeSmuME emulator
+flatpak install flathub org.desmume.DeSmuME
+# Install RPCS3 emulator
+flatpak install flathub net.rpcs3.RPCS3
+# Install Yuzu emulator
+flatpak install flathub org.yuzu_emu.yuzu
+# Install xemu emulator
+flatpak install flathub app.xemu.xemu
+# Install m64plus emulator
+flatpak install flathub io.github.m64p.m64p
+# Install mGBA emulator
+flatpak install flathub io.mgba.mGBA
+# Install Dolphin emulator
+flatpak install flathub org.DolphinEmu.dolphin-emu
+# Install DOSBOX emulator
+flatpak install flathub io.github.dosbox-staging
+# Install DOSBOX-X emulator
+flatpak install flathub com.dosbox_x.DOSBox-X
+# Install Scumm DOS emulator emulator
+flatpak install flathub org.scummvm.ScummVM
+# Install SNES emulator
+flatpak install flathub com.snes9x.Snes9x
