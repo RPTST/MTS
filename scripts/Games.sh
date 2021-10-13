e617636b9bdf6eb5ed2787dfaf82519fd88be5fb
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 18:53:26 -0400

    Create Games.sh

---
+++ scripts/Games.sh
@@ -0,0 +1,16 @@
+# Install flatpack games
+#
+# Install Minetest a free minecraft alt
+flatpak install flathub net.minetest.Minetest
+# Install 0 A.D. (updated)
+flatpak install flathub com.play0ad.zeroad
+# Install SuperTuxKart
+flatpak install flathub net.supertuxkart.SuperTuxKart
+# Install CIV clone
+flatpak install flathub org.freeciv.Freeciv
+# Install train simulator
+flatpak install flathub org.libretrainsim.Libre-TrainSim
+# FPS
+flatpak install flathub com.realm667.Wolfenstein_Blade_of_Agony
+# install Steam
+flatpak install flathub com.valvesoftware.Steam
