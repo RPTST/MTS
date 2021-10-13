cf1a4341adb203d25c872c5c355bda65751c46f3
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 18:54:27 -0400

    Create KDE_games.sh

---
+++ scripts/KDE_games.sh
@@ -0,0 +1,11 @@
+# Install KDE flatpack games
+#
+#
+# Install PACMAN clone
+flatpak install flathub org.kde.kapman
+# Install Tetris clone
+flatpak install flathub org.kde.kblocks
+# Install katomic
+flatpak install flathub org.kde.katomic
+# Install card games
+flatpak install flathub org.kde.kpat
