07aabdf125e38a0332fe5aac98a0369b7b9e718c
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-14 01:27:19 -0400

    Update and rename scripts/0-A_D.sh to scripts/flatpack/flatpack_install.sh

--- scripts/flatpack/flatpack_install.sh
+++ scripts/flatpack/flatpack_install.sh
@@ -1,5 +1,6 @@
 # Install 0 A.D. (updated)
-sudo apt install flatpak
+sudo apt update
+sudo apt upgrade -y
+sudo apt install flatpak -y
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo reboot
-flatpak install flathub com.play0ad.zeroad
