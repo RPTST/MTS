03cc7702adfc2b08fab37a48c0775cb5a00d2ce8
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-14 18:36:49 -0400

    Create Standard_Apps.sh

---
+++ scripts/Standard_Apps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository ppa:pbek/qownnotes
+sudo add-apt-repository ppa:d-bannon/ppa-tomboy-ng
+sudo apt install ubuntu-restricted-extras Darktable Celluloid Parole Pinta krita mypaint gparted gdebi tomboy-ng qownnotes
