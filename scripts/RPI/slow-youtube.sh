1e1308c119bfee3993e8df9da661fcf4a02e98c1
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 20:57:44 -0400

    Create slow-youtube.sh

---
+++ scripts/RPI/slow-youtube.sh
@@ -0,0 +1,25 @@
+# slow-youtube
+sudo apt install python3 python3-pip mpv firefox
+python3 -m pip install --user --upgrade youtube-dl
+mkdir ~/projects/
+cd ~/projects/
+wget https://github.com/akiirui/mpv-handler/releases/latest/download/mpv-handler-linux-x64.zip
+unzip -d mpv-handler mpv-handler-linux-x64.zip
+mkdir ~/.local/bin/ #just in case
+mkdir ~/.local/share/applications/ #just in case
+cp mpv-handler/mpv-handler ~/.local/bin/
+cp mpv-handler/mpv-handler.desktop ~/.local/share/applications/
+mkdir ~/.config/mpv-handler
+cp mpv-handler/config.toml ~/.config/mpv-handler/
+
+#For Firefox
+cd ~/projects/
+wget https://github.com/RPTST/MTS/blob/main/src/aecec67f-0d10-4fa7-b7c7-609a2db280cf.zip
+cp aecec67f-0d10-4fa7-b7c7-609a2db280cf.zip ~/.mozilla/extensions/
+cd ~/.mozilla/extensions/
+unzip aecec67f-0d10-4fa7-b7c7-609a2db280cf.zip
+
+#Register the new URL scheme handler with XDG
+xdg-mime default mpv-handler.desktop x-scheme-handler/mpv
+
+# And that's it. Open a video on YouTube and you should see a new floating button in the bottom left-hand side of the website. Click it and the web browser should ask you if you want to allow the site to open mpv links:
