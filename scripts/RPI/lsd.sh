7de2b498bab904fc611bade497b47ed426e874e8
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 21:09:49 -0400

    Create lsd.sh

---
+++ scripts/RPI/lsd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+filename=".bashrc"
+search="ls --color=auto"
+replace="lsd"
+
+     mkdir $HOME/projects
+     cd $HOME/projects/
+     wget -P ~/projects https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_arm64.deb
+     sudo dpkg -i lsd-musl_0.20.1_arm64.deb
+     sed -i "s/$search/$replace/" $HOME/$filename
