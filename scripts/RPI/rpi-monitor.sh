9e5d3f4a17dc5da1f04f802485552be2743fb8f0
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-13 21:06:48 -0400

    Create rpi-monitor.sh

---
+++ scripts/RPI/rpi-monitor.sh
@@ -0,0 +1,11 @@
+# Install RPI monitor
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2C0D3C0F
+sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
+sudo apt update
+sudo apt install -y rpimonitor
+sudo service rpimonitor stop
+sudo service rpimonitor start
+sudo service rpimonitor restart
+sudo service rpimonitor status
+echo -e "Open a browser"
+echo -e "http://<IP_ADDRESS>:8888"
