fa1b3cc39fd30638d12c269d77d6c4483e911849
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-14 04:07:30 -0400

    Create all_snaps.sh

---
+++ scripts/snap/all_snaps.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+sudo snap install android-studio --classic
+sudo snap install solitaire
+sudo snap install gog-galaxy-wine
+sudo snap install cncra
+sudo snap install cncra2yr
+sudo snap install tic-tac-toe
+sudo snap install cnctsun
+sudo snap install openjk
+sudo snap install os-runes
+sudo snap install minesweeper
+sudo snap install lsd
+sudo snap install powershell --classic
+sudo snap install node --classic
+sudo snap install go --classic
+sudo snap install node-red
+sudo snap install leafpad
+sudo snap install node-red-rpi-lwl
+sudo snap install slack --classic
+sudo snap install skype
+sudo snap install discord
+sudo snap install thunderbird
+sudo snap install zoom-client
+sudo snap install teams-for-linux
+sudo snap install brave
+sudo snap install firefox
+sudo snap install libreoffice
+sudo snap install simplenote
+sudo snap install teams-insiders
+sudo snap install gnome-calendar
+sudo snap install remmina
+sudo snap install ungoogled-chromium
+sudo snap install mumble
+sudo snap install ghex-udt
+sudo snap install gnome-system-monitor
+sudo snap install htop
+sudo snap install gotop
+sudo snap install tio --classic
+sudo snap install sftpclient
+sudo snap install gnome-clocks
+sudo snap install gnome-calculator
+sudo snap install ark
+sudo snap install bashtop
+sudo snap install android-file-transfer-linux
+sudo snap install zoom-client
+sudo snap install cool-retro-term --classic
+sudo snap install ffmpeg
+sudo snap install rpi-imager
+sudo snap install bpytop
+sudo snap install keepassxc
+sudo snap install backslash-maps
+sudo snap install termshark
+sudo snap install gdu-disk-usage-analyzer
+sudo snap install shotcut --classic
+sudo snap install handbrake-jz
+sudo snap install mpv --beta
+sudo snap install obs-studio
+sudo snap install kdenlive
+sudo snap install gimp
+sudo snap install mosquitto
+sudo snap install aws-cli --classic
+sudo snap install nextcloud-nextbox
+sudo snap install winbox
+sudo snap install unifi --edge
+sudo snap install 1password
+sudo snap install adguard-home
+sudo snap install mycroft --beta
+sudo snap install octoprint-pfs --edge
+sudo snap install clementine
+sudo snap install google-play-music-desktop-player
+sudo snap install rhyme
+sudo snap install audible-for-linux
+sudo snap install mycroft-precise --edge
+sudo snap install moonlight
+sudo snap install youtube-dl
+sudo snap install freecad
+sudo snap install meshlab
+sudo snap install inkscape
+sudo snap install blender --classic
+sudo snap install openscad
+sudo snap install gnome-dictionary
+sudo snap install kgeography
+sudo snap install gnome-calculator
+sudo snap install kcalc
+sudo snap install wethr
+sudo snap install openrocket --edge
+sudo snap install sublime-text --classic
+sudo snap install atom --classic
+sudo snap install notepad-plus-plus
+sudo snap install emacs --classic
+sudo snap install vim-editor --beta
+sudo snap install micropython
+sudo snap install sublime-merge --classic
+sudo snap install polar-bookshelf
