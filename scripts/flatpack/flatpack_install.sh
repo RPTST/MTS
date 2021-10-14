2657df0a2a9aecee73ed4d023a5de6067251ca52
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-14 01:27:43 -0400

    Update flatpack_install.sh

--- scripts/flatpack/flatpack_install.sh
+++ scripts/flatpack/flatpack_install.sh
@@ -1,6 +1,8 @@
-# Install 0 A.D. (updated)
+# Install flatpack
+#
 sudo apt update
 sudo apt upgrade -y
 sudo apt install flatpak -y
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo reboot
+#
