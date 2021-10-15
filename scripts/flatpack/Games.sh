70858c259bef8f2994d1f7d19df7ba68864f8682
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-15 18:35:10 -0400

    Update Games.sh

--- scripts/flatpack/Games.sh
+++ scripts/flatpack/Games.sh
@@ -1,18 +1,18 @@
 # Install flatpack games
 #
 # Install Minetest a free minecraft alt
-flatpak install flathub net.minetest.Minetest
+flatpak install -y --noninteractive flathub net.minetest.Minetest
 # Install 0 A.D. (updated)
-flatpak install flathub com.play0ad.zeroad
+flatpak install -y --noninteractive flathub com.play0ad.zeroad
 # Install SuperTuxKart
-flatpak install flathub net.supertuxkart.SuperTuxKart
+flatpak install -y --noninteractive flathub net.supertuxkart.SuperTuxKart
 # Install CIV clone
-flatpak install flathub org.freeciv.Freeciv
+flatpak install -y --noninteractive flathub org.freeciv.Freeciv
 # Install train simulator
-flatpak install flathub org.libretrainsim.Libre-TrainSim
+flatpak install -y --noninteractive flathub org.libretrainsim.Libre-TrainSim
 # FPS
-flatpak install flathub com.realm667.Wolfenstein_Blade_of_Agony
+flatpak install -y --noninteractive flathub com.realm667.Wolfenstein_Blade_of_Agony
 # install Steam
-flatpak install flathub com.valvesoftware.Steam
+flatpak install -y --noninteractive flathub com.valvesoftware.Steam
 # benchmark
-flatpak install flathub com.basemark.BasemarkGPU
+flatpak install -y --noninteractive flathub com.basemark.BasemarkGPU
