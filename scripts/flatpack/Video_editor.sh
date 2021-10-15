9b95b46bf0e08afae365c1afcf5da3b294735b65
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-15 18:36:11 -0400

    Update Video_editor.sh

--- scripts/flatpack/Video_editor.sh
+++ scripts/flatpack/Video_editor.sh
@@ -1,55 +1,55 @@
 # Standard video install
 #
 # Install graphics & videos
-flatpak install flathub org.gimp.GIMP
-flatpak install flathub org.kde.kdenlive
-flatpak install flathub org.shotcut.Shotcut
-flatpak install flathub org.gnome.Cheese
-flatpak install flathub fr.handbrake.ghb
-flatpak install flathub com.makemkv.MakeMKV
-flatpak install flathub io.mpv.Mpv
-flatpak install flathub org.videolan.VLC
-flatpak install flathub com.vinszent.GnomeTwitch
-flatpak install flathub com.obsproject.Studio
-flatpak install flathub org.tuxpaint.Tuxpaint
-flatpak install flathub org.blender.Blender
-flatpak install flathub org.flameshot.Flameshot
-flatpak install flathub org.freecadweb.FreeCAD
-flatpak install flathub org.inkscape.Inkscape
-flatpak install flathub org.kde.krita
-flatpak install flathub net.meshlab.MeshLab
-flatpak install flathub org.openscad.OpenSCAD
-flatpak install flathub com.rawtherapee.RawTherapee
-flatpak install flathub org.gnome.Shotwell
-flatpak install flathub com.hamrick.VueScan
-flatpak install flathub io.github.JaGoLi.ytdl_gui
+flatpak install -y --noninteractive  flathub org.gimp.GIMP
+flatpak install -y --noninteractive  flathub org.kde.kdenlive
+flatpak install -y --noninteractive  flathub org.shotcut.Shotcut
+flatpak install -y --noninteractive  flathub org.gnome.Cheese
+flatpak install -y --noninteractive  flathub fr.handbrake.ghb
+flatpak install -y --noninteractive  flathub com.makemkv.MakeMKV
+flatpak install -y --noninteractive  flathub io.mpv.Mpv
+flatpak install -y --noninteractive  flathub org.videolan.VLC
+flatpak install -y --noninteractive  flathub com.vinszent.GnomeTwitch
+flatpak install -y --noninteractive  flathub com.obsproject.Studio
+flatpak install -y --noninteractive  flathub org.tuxpaint.Tuxpaint
+flatpak install -y --noninteractive  flathub org.blender.Blender
+flatpak install -y --noninteractive  flathub org.flameshot.Flameshot
+flatpak install -y --noninteractive  flathub org.freecadweb.FreeCAD
+flatpak install -y --noninteractive  flathub org.inkscape.Inkscape
+flatpak install -y --noninteractive  flathub org.kde.krita
+flatpak install -y --noninteractive  flathub net.meshlab.MeshLab
+flatpak install -y --noninteractive  flathub org.openscad.OpenSCAD
+flatpak install -y --noninteractive  flathub com.rawtherapee.RawTherapee
+flatpak install -y --noninteractive  flathub org.gnome.Shotwell
+flatpak install -y --noninteractive  flathub com.hamrick.VueScan
+flatpak install -y --noninteractive  flathub io.github.JaGoLi.ytdl_gui
 #
 # Install Chat
-flatpak install flathub com.slack.Slack
-flatpak install flathub im.pidgin.Pidgin
-flatpak install flathub io.github.Hexchat
-flatpak install flathub com.microsoft.Teams
+flatpak install -y --noninteractive  flathub com.slack.Slack
+flatpak install -y --noninteractive  flathub im.pidgin.Pidgin
+flatpak install -y --noninteractive  flathub io.github.Hexchat
+flatpak install -y --noninteractive  flathub com.microsoft.Teams
 #
 # Install video conference
-flatpak install flathub com.skype.Client
-flatpak install flathub us.zoom.Zoom
-flatpak install flathub org.jitsi.jitsi-meet
-flatpak install flathub com.bluejeans.BlueJeans
+flatpak install -y --noninteractive  flathub com.skype.Client
+flatpak install -y --noninteractive  flathub us.zoom.Zoom
+flatpak install -y --noninteractive  flathub org.jitsi.jitsi-meet
+flatpak install -y --noninteractive  flathub com.bluejeans.BlueJeans
 #
 # Install voice conference
-flatpak install flathub info.mumble.Mumble
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.teamspeak.TeamSpeak
+flatpak install -y --noninteractive  flathub info.mumble.Mumble
+flatpak install -y --noninteractive  flathub com.discordapp.Discord
+flatpak install -y --noninteractive  flathub com.teamspeak.TeamSpeak
 #
 # Install mail
-flatpak install flathub ch.protonmail.protonmail-bridge
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub ch.protonmail.protonmail-import-export-app
+flatpak install -y --noninteractive  flathub ch.protonmail.protonmail-bridge
+flatpak install -y --noninteractive  flathub org.mozilla.Thunderbird
+flatpak install -y --noninteractive  flathub ch.protonmail.protonmail-import-export-app
 #
 # Install audio
-flatpak install flathub org.ardour.Ardour
-flatpak install flathub org.atheme.audacious
-flatpak install flathub org.clementine_player.Clementine
-flatpak install flathub com.github.wwmm.easyeffects
-flatpak install flathub com.googleplaymusicdesktopplayer.GPMDP
-flatpak install flathub org.gnome.Rhythmbox3
+flatpak install -y --noninteractive  flathub org.ardour.Ardour
+flatpak install -y --noninteractive  flathub org.atheme.audacious
+flatpak install -y --noninteractive  flathub org.clementine_player.Clementine
+flatpak install -y --noninteractive  flathub com.github.wwmm.easyeffects
+flatpak install -y --noninteractive  flathub com.googleplaymusicdesktopplayer.GPMDP
+flatpak install -y --noninteractive  flathub org.gnome.Rhythmbox3
