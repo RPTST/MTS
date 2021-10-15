64d2485745108e2c0f587081da66ed222454a3a9
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-15 18:34:30 -0400

    Update Game_emulator.sh

--- scripts/flatpack/Game_emulator.sh
+++ scripts/flatpack/Game_emulator.sh
@@ -1,32 +1,32 @@
 # Standard Game emulators install
 #
 # RetroArch enables you to run classic games on a wide range of computers and consoles
-flatpak install flathub org.libretro.RetroArch
+flatpak install -y --noninteractive flathub org.libretro.RetroArch
 # Playstation 2 'emulator
-flatpak install flathub net.pcsx2.PCSX2
+flatpak install -y --noninteractive flathub net.pcsx2.PCSX2
 # PSP emulator
-flatpak install flathub org.ppsspp.PPSSPP
+flatpak install -y --noninteractive flathub org.ppsspp.PPSSPP
 # Install DeSmuME emulator
-flatpak install flathub org.desmume.DeSmuME
+flatpak install -y --noninteractive flathub org.desmume.DeSmuME
 # Install RPCS3 emulator
-flatpak install flathub net.rpcs3.RPCS3
+flatpak install -y --noninteractive flathub net.rpcs3.RPCS3
 # Install Yuzu emulator
-flatpak install flathub org.yuzu_emu.yuzu
+flatpak install -y --noninteractive flathub org.yuzu_emu.yuzu
 # Install xemu emulator
-flatpak install flathub app.xemu.xemu
+flatpak install -y --noninteractive flathub app.xemu.xemu
 # Install m64plus emulator
-flatpak install flathub io.github.m64p.m64p
+flatpak install -y --noninteractive flathub io.github.m64p.m64p
 # Install mGBA emulator
-flatpak install flathub io.mgba.mGBA
+flatpak install -y --noninteractive flathub io.mgba.mGBA
 # Install Dolphin emulator
-flatpak install flathub org.DolphinEmu.dolphin-emu
+flatpak install -y --noninteractive flathub org.DolphinEmu.dolphin-emu
 # Install DOSBOX emulator
-flatpak install flathub io.github.dosbox-staging
+flatpak install -y --noninteractive flathub io.github.dosbox-staging
 # Install DOSBOX-X emulator
-flatpak install flathub com.dosbox_x.DOSBox-X
+flatpak install -y --noninteractive flathub com.dosbox_x.DOSBox-X
 # Install Scumm DOS emulator emulator
-flatpak install flathub org.scummvm.ScummVM
+flatpak install -y --noninteractive flathub org.scummvm.ScummVM
 # Install SNES emulator
-flatpak install flathub com.snes9x.Snes9x
+flatpak install -y --noninteractive flathub com.snes9x.Snes9x
 # benchmark
-flatpak install flathub com.basemark.BasemarkGPU
+flatpak install -y --noninteractive flathub com.basemark.BasemarkGPU
