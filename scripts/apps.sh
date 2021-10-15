6083b7934fc6a721a72ae3a2fc133ae65334aa27
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-14 20:19:56 -0400

    Update apps.sh

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -16,11 +16,11 @@ fi
 if [ $SUDO_USER ]; then
     real_user=$SUDO_USER
     # Install Terminal Apps
-    apt install -y tshark saidar xlsx2csv docx2txt pwgen libcrack2 htop bmon wavemon iftop ipcalc hexcurse exa hping3 arping lshw fping chkrootkit ioping trash-cli ranger mc whowatch lsof nethogs fdupes stress ccze tilde nmap
+    apt install -y dpkg-dev build-essential debhelper tshark saidar xlsx2csv docx2txt pwgen libcrack2 htop bmon wavemon iftop ipcalc hexcurse exa hping3 arping lshw fping chkrootkit ioping trash-cli ranger mc whowatch lsof nethogs fdupes stress ccze tilde nmap
     # Install Desktop Apps
     add-apt-repository ppa:pbek/qownnotes
     add-apt-repository ppa:d-bannon/ppa-tomboy-ng
-    apt install darktable celluloid parole pinta krita mypaint gparted gdebi tomboy-ng qownnotes
+    apt install darktable celluloid parole pinta krita mypaint gparted gdebi tomboy-ng qownnotessudo
     
 else
     real_user=$(whoami)
