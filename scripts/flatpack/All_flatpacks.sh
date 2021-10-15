2cffa7a59375a4f460561808b421baa165332e42
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-15 18:37:22 -0400

    Update All_flatpacks.sh

--- scripts/flatpack/All_flatpacks.sh
+++ scripts/flatpack/All_flatpacks.sh
@@ -1,194 +1,194 @@
 # Install flatpack game emulators
 # RetroArch enables you to run classic games on a wide range of computers and consoles
-flatpak install flathub org.libretro.RetroArch
+flatpak install -y --noninteractive  flathub org.libretro.RetroArch
 # Playstation 2 'emulator
-flatpak install flathub net.pcsx2.PCSX2
+flatpak install -y --noninteractive  flathub net.pcsx2.PCSX2
 # PSP emulator
-flatpak install flathub org.ppsspp.PPSSPP
+flatpak install -y --noninteractive  flathub org.ppsspp.PPSSPP
 # Install DeSmuME emulator
-flatpak install flathub org.desmume.DeSmuME
+flatpak install -y --noninteractive  flathub org.desmume.DeSmuME
 # Install RPCS3 emulator
-flatpak install flathub net.rpcs3.RPCS3
+flatpak install -y --noninteractive  flathub net.rpcs3.RPCS3
 # Install Yuzu emulator
-flatpak install flathub org.yuzu_emu.yuzu
+flatpak install -y --noninteractive  flathub org.yuzu_emu.yuzu
 # Install xemu emulator
-flatpak install flathub app.xemu.xemu
-flatpak install flathub io.github.m64p.m64p
-flatpak install flathub io.mgba.mGBA
-flatpak install flathub org.DolphinEmu.dolphin-emu
-flatpak install flathub io.github.dosbox-staging
-flatpak install flathub com.dosbox_x.DOSBox-X
-flatpak install flathub org.scummvm.ScummVM
-flatpak install flathub com.snes9x.Snes9x
+flatpak install -y --noninteractive  flathub app.xemu.xemu
+flatpak install -y --noninteractive  flathub io.github.m64p.m64p
+flatpak install -y --noninteractive  flathub io.mgba.mGBA
+flatpak install -y --noninteractive  flathub org.DolphinEmu.dolphin-emu
+flatpak install -y --noninteractive  flathub io.github.dosbox-staging
+flatpak install -y --noninteractive  flathub com.dosbox_x.DOSBox-X
+flatpak install -y --noninteractive  flathub org.scummvm.ScummVM
+flatpak install -y --noninteractive  flathub com.snes9x.Snes9x
 
 
 #unknown emulators
-flatpak install flathub dev.bsnes.bsnes
-flatpak install flathub org.citra_emu.citra
-flatpak install flathub org.duckstation.DuckStation
-flatpak install flathub org.flycast.Flycast
-flatpak install flathub org.gnome.Games
-flatpak install flathub com.github.shonumi.gbe-plus
-flatpak install flathub net.kuribo64.melonDS
+flatpak install -y --noninteractive  flathub dev.bsnes.bsnes
+flatpak install -y --noninteractive  flathub org.citra_emu.citra
+flatpak install -y --noninteractive  flathub org.duckstation.DuckStation
+flatpak install -y --noninteractive  flathub org.flycast.Flycast
+flatpak install -y --noninteractive  flathub org.gnome.Games
+flatpak install -y --noninteractive  flathub com.github.shonumi.gbe-plus
+flatpak install -y --noninteractive  flathub net.kuribo64.melonDS
 
 # benchmark
-flatpak install flathub com.basemark.BasemarkGPU
+flatpak install -y --noninteractive  flathub com.basemark.BasemarkGPU
 
 # Install flatpack games
 # Install Minetest a free minecraft alt
-flatpak install flathub net.minetest.Minetest
+flatpak install -y --noninteractive  flathub net.minetest.Minetest
 # Install 0 A.D. (updated)
-flatpak install flathub com.play0ad.zeroad
+flatpak install -y --noninteractive  flathub com.play0ad.zeroad
 # Install SuperTuxKart
-flatpak install flathub net.supertuxkart.SuperTuxKart
+flatpak install -y --noninteractive  flathub net.supertuxkart.SuperTuxKart
 # Install Solitar
-flatpak install flathub org.gnome.Aisleriot
+flatpak install -y --noninteractive  flathub org.gnome.Aisleriot
 # Install gnome Chess
-flatpak install flathub org.gnome.Chess
+flatpak install -y --noninteractive  flathub org.gnome.Chess
 # Install CIV clone
-flatpak install flathub org.freeciv.Freeciv
+flatpak install -y --noninteractive  flathub org.freeciv.Freeciv
 # Install gnome Mahjongg
-flatpak install flathub org.gnome.Mahjongg
+flatpak install -y --noninteractive  flathub org.gnome.Mahjongg
 # Install gnome Sudoku
-flatpak install flathub org.gnome.Sudoku
+flatpak install -y --noninteractive  flathub org.gnome.Sudoku
 # Install gnome Reversi
-flatpak install flathub org.gnome.Reversi
+flatpak install -y --noninteractive  flathub org.gnome.Reversi
 # Install PACMAN clone
-flatpak install flathub org.kde.kapman
+flatpak install -y --noninteractive  flathub org.kde.kapman
 # Install Tetris clone
-flatpak install flathub org.kde.kblocks
+flatpak install -y --noninteractive  flathub org.kde.kblocks
 # Install katomic
-flatpak install flathub org.kde.katomic
+flatpak install -y --noninteractive  flathub org.kde.katomic
 # Install card games
-flatpak install flathub org.kde.kpat
+flatpak install -y --noninteractive  flathub org.kde.kpat
 # Install train simulator
-flatpak install flathub org.libretrainsim.Libre-TrainSim
+flatpak install -y --noninteractive  flathub org.libretrainsim.Libre-TrainSim
 # FPS
-flatpak install flathub com.realm667.Wolfenstein_Blade_of_Agony
+flatpak install -y --noninteractive  flathub com.realm667.Wolfenstein_Blade_of_Agony
 # install Steam
-flatpak install flathub com.valvesoftware.Steam
+flatpak install -y --noninteractive  flathub com.valvesoftware.Steam
 
 
 # Install Productivity
-flatpak install flathub org.xfce.mousepad
-flatpak install flathub com.abisource.AbiWord
-flatpak install flathub org.libreoffice.LibreOffice
-flatpak install flathub org.gnome.gedit
-flatpak install flathub org.gnome.Gnote
-flatpak install flathub org.gnome.Notes
-flatpak install flathub org.gnome.TextEditor
+flatpak install -y --noninteractive  flathub org.xfce.mousepad
+flatpak install -y --noninteractive  flathub com.abisource.AbiWord
+flatpak install -y --noninteractive  flathub org.libreoffice.LibreOffice
+flatpak install -y --noninteractive  flathub org.gnome.gedit
+flatpak install -y --noninteractive  flathub org.gnome.Gnote
+flatpak install -y --noninteractive  flathub org.gnome.Notes
+flatpak install -y --noninteractive  flathub org.gnome.TextEditor
 
 # Install Apps
-flatpak install flathub com.leinardi.gkraken
-flatpak install flathub org.telegram.desktop
-flatpak install flathub org.remmina.Remmina
-flatpak install flathub org.filezillaproject.Filezilla
-flatpak install flathub com.poweriso.PowerISO
-flatpak install flathub com.github.jkotra.eovpn
-flatpak install flathub org.kde.ktorrent
-flatpak install flathub nz.mega.MEGAsync
-flatpak install flathub com.cassidyjames.plausible
-flatpak install flathub org.qbittorrent.qBittorrent
-flatpak install flathub com.synology.CloudStationBackup
-flatpak install flathub com.synology.SynologyDrive
-flatpak install flathub io.bit3.WhatsAppQT
-flatpak install flathub org.wireshark.Wireshark
-flatpak install flathub com.calibre_ebook.calibre
-flatpak install flathub org.kde.dolphin
-flatpak install flathub io.github.arunsivaramanneo.GPUViewer
-flatpak install flathub io.github.peazip.PeaZip
-flatpak install flathub io.github.prateekmedia.appimagepool
-flatpak install flathub org.kde.ark
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub com.github.hluk.copyq
-flatpak install flathub org.kde.kclock
-flatpak install flathub org.gnome.Extensions
-flatpak install flathub org.gnome.FontManager
-flatpak install flathub org.gustavoperedo.FontDownloader
-flatpak install flathub io.github.mmstick.FontFinder
-flatpak install flathub org.gnome.Calculator
-flatpak install flathub org.gnome.Maps
-flatpak install flathub org.gnome.clocks
-flatpak install flathub org.keepassxc.KeePassXC
-flatpak install flathub com.github.calo001.luna
-flatpak install flathub com.nextcloud.desktopclient.nextcloud
-flatpak install flathub org.raspberrypi.rpi-imager
-flatpak install flathub com.simplenote.Simplenote
+flatpak install -y --noninteractive  flathub com.leinardi.gkraken
+flatpak install -y --noninteractive  flathub org.telegram.desktop
+flatpak install -y --noninteractive  flathub org.remmina.Remmina
+flatpak install -y --noninteractive  flathub org.filezillaproject.Filezilla
+flatpak install -y --noninteractive  flathub com.poweriso.PowerISO
+flatpak install -y --noninteractive  flathub com.github.jkotra.eovpn
+flatpak install -y --noninteractive  flathub org.kde.ktorrent
+flatpak install -y --noninteractive  flathub nz.mega.MEGAsync
+flatpak install -y --noninteractive  flathub com.cassidyjames.plausible
+flatpak install -y --noninteractive  flathub org.qbittorrent.qBittorrent
+flatpak install -y --noninteractive  flathub com.synology.CloudStationBackup
+flatpak install -y --noninteractive  flathub com.synology.SynologyDrive
+flatpak install -y --noninteractive  flathub io.bit3.WhatsAppQT
+flatpak install -y --noninteractive  flathub org.wireshark.Wireshark
+flatpak install -y --noninteractive  flathub com.calibre_ebook.calibre
+flatpak install -y --noninteractive  flathub org.kde.dolphin
+flatpak install -y --noninteractive  flathub io.github.arunsivaramanneo.GPUViewer
+flatpak install -y --noninteractive  flathub io.github.peazip.PeaZip
+flatpak install -y --noninteractive  flathub io.github.prateekmedia.appimagepool
+flatpak install -y --noninteractive  flathub org.kde.ark
+flatpak install -y --noninteractive  flathub com.bitwarden.desktop
+flatpak install -y --noninteractive  flathub com.github.hluk.copyq
+flatpak install -y --noninteractive  flathub org.kde.kclock
+flatpak install -y --noninteractive  flathub org.gnome.Extensions
+flatpak install -y --noninteractive  flathub org.gnome.FontManager
+flatpak install -y --noninteractive  flathub org.gustavoperedo.FontDownloader
+flatpak install -y --noninteractive  flathub io.github.mmstick.FontFinder
+flatpak install -y --noninteractive  flathub org.gnome.Calculator
+flatpak install -y --noninteractive  flathub org.gnome.Maps
+flatpak install -y --noninteractive  flathub org.gnome.clocks
+flatpak install -y --noninteractive  flathub org.keepassxc.KeePassXC
+flatpak install -y --noninteractive  flathub com.github.calo001.luna
+flatpak install -y --noninteractive  flathub com.nextcloud.desktopclient.nextcloud
+flatpak install -y --noninteractive  flathub org.raspberrypi.rpi-imager
+flatpak install -y --noninteractive  flathub com.simplenote.Simplenote
 
 #Store
-flatpak install flathub io.github.prateekmedia.appimagepool
+flatpak install -y --noninteractive  flathub io.github.prateekmedia.appimagepool
 
 # Install VM manager
-flatpak install flathub org.gnome.Boxes
+flatpak install -y --noninteractive  flathub org.gnome.Boxes
 
 # Install browser
-flatpak install flathub org.gnome.Epiphany
-flatpak install flathub org.mozilla.firefox
-flatpak install flathub io.gitlab.librewolf-community
-flatpak install flathub com.github.Eloston.UngoogledChromium
+flatpak install -y --noninteractive  flathub org.gnome.Epiphany
+flatpak install -y --noninteractive  flathub org.mozilla.firefox
+flatpak install -y --noninteractive  flathub io.gitlab.librewolf-community
+flatpak install -y --noninteractive  flathub com.github.Eloston.UngoogledChromium
 
 # Install graphics & videos
-flatpak install flathub org.gimp.GIMP
-flatpak install flathub org.kde.kdenlive
-flatpak install flathub org.shotcut.Shotcut
-flatpak install flathub org.gnome.Cheese
-flatpak install flathub fr.handbrake.ghb
-flatpak install flathub com.makemkv.MakeMKV
-flatpak install flathub io.mpv.Mpv
-flatpak install flathub org.videolan.VLC
-flatpak install flathub com.vinszent.GnomeTwitch
-flatpak install flathub com.obsproject.Studio
-flatpak install flathub org.tuxpaint.Tuxpaint
-flatpak install flathub org.blender.Blender
-flatpak install flathub org.flameshot.Flameshot
-flatpak install flathub org.freecadweb.FreeCAD
-flatpak install flathub org.inkscape.Inkscape
-flatpak install flathub org.kde.krita
-flatpak install flathub net.meshlab.MeshLab
-flatpak install flathub org.openscad.OpenSCAD
-flatpak install flathub com.rawtherapee.RawTherapee
-flatpak install flathub org.gnome.Shotwell
-flatpak install flathub com.hamrick.VueScan
-flatpak install flathub io.github.JaGoLi.ytdl_gui
-flatpak install flathub im.kaidan.kaida
+flatpak install -y --noninteractive  flathub org.gimp.GIMP
+flatpak install -y --noninteractive  flathub org.kde.kdenlive
+flatpak install -y --noninteractive  flathub org.shotcut.Shotcut
+flatpak install -y --noninteractive  flathub org.gnome.Cheese
+flatpak install -y --noninteractive  flathub fr.handbrake.ghb
+flatpak install -y --noninteractive  flathub com.makemkv.MakeMKV
+flatpak install -y --noninteractive  flathub io.mpv.Mpv
+flatpak install -y --noninteractive  flathub org.videolan.VLC
+flatpak install -y --noninteractive  flathub com.vinszent.GnomeTwitch
+flatpak install -y --noninteractive  flathub com.obsproject.Studio
+flatpak install -y --noninteractive  flathub org.tuxpaint.Tuxpaint
+flatpak install -y --noninteractive  flathub org.blender.Blender
+flatpak install -y --noninteractive  flathub org.flameshot.Flameshot
+flatpak install -y --noninteractive  flathub org.freecadweb.FreeCAD
+flatpak install -y --noninteractive  flathub org.inkscape.Inkscape
+flatpak install -y --noninteractive  flathub org.kde.krita
+flatpak install -y --noninteractive  flathub net.meshlab.MeshLab
+flatpak install -y --noninteractive  flathub org.openscad.OpenSCAD
+flatpak install -y --noninteractive  flathub com.rawtherapee.RawTherapee
+flatpak install -y --noninteractive  flathub org.gnome.Shotwell
+flatpak install -y --noninteractive  flathub com.hamrick.VueScan
+flatpak install -y --noninteractive  flathub io.github.JaGoLi.ytdl_gui
+flatpak install -y --noninteractive  flathub im.kaidan.kaida
 
 # Install Developer Tools 
-flatpak install flathub com.vscodium.codium
-flatpak install flathub org.geany.Geany
-flatpak install flathub io.github.shiftey.Desktop
-flatpak install flathub org.gnu.emacs
-flatpak install flathub com.notepadqq.Notepadqq
-flatpak install flathub com.sublimetext.three
-flatpak install flathub io.atom.Atom
-flatpak install flathub org.vim.Vim
-flatpak install flathub org.librepcb.LibrePCB
+flatpak install -y --noninteractive  flathub com.vscodium.codium
+flatpak install -y --noninteractive  flathub org.geany.Geany
+flatpak install -y --noninteractive  flathub io.github.shiftey.Desktop
+flatpak install -y --noninteractive  flathub org.gnu.emacs
+flatpak install -y --noninteractive  flathub com.notepadqq.Notepadqq
+flatpak install -y --noninteractive  flathub com.sublimetext.three
+flatpak install -y --noninteractive  flathub io.atom.Atom
+flatpak install -y --noninteractive  flathub org.vim.Vim
+flatpak install -y --noninteractive  flathub org.librepcb.LibrePCB
 
 # Install Chat
-flatpak install flathub com.slack.Slack
-flatpak install flathub im.pidgin.Pidgin
-flatpak install flathub io.github.Hexchat
-flatpak install flathub com.microsoft.Teams
+flatpak install -y --noninteractive  flathub com.slack.Slack
+flatpak install -y --noninteractive  flathub im.pidgin.Pidgin
+flatpak install -y --noninteractive  flathub io.github.Hexchat
+flatpak install -y --noninteractive  flathub com.microsoft.Teams
 
 # Install video conference
-flatpak install flathub com.skype.Client
-flatpak install flathub us.zoom.Zoom
-flatpak install flathub org.jitsi.jitsi-meet
-flatpak install flathub com.bluejeans.BlueJeans
+flatpak install -y --noninteractive  flathub com.skype.Client
+flatpak install -y --noninteractive  flathub us.zoom.Zoom
+flatpak install -y --noninteractive  flathub org.jitsi.jitsi-meet
+flatpak install -y --noninteractive  flathub com.bluejeans.BlueJeans
 
 # Install voice conference
-flatpak install flathub info.mumble.Mumble
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.teamspeak.TeamSpeak
+flatpak install -y --noninteractive  flathub info.mumble.Mumble
+flatpak install -y --noninteractive  flathub com.discordapp.Discord
+flatpak install -y --noninteractive  flathub com.teamspeak.TeamSpeak
 
 # Install mail
-flatpak install flathub ch.protonmail.protonmail-bridge
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub ch.protonmail.protonmail-import-export-app
+flatpak install -y --noninteractive  flathub ch.protonmail.protonmail-bridge
+flatpak install -y --noninteractive  flathub org.mozilla.Thunderbird
+flatpak install -y --noninteractive  flathub ch.protonmail.protonmail-import-export-app
 
 # Install audio
-flatpak install flathub org.ardour.Ardour
-flatpak install flathub org.atheme.audacious
-flatpak install flathub org.clementine_player.Clementine
-flatpak install flathub com.github.wwmm.easyeffects
-flatpak install flathub com.googleplaymusicdesktopplayer.GPMDP
-flatpak install flathub org.gnome.Rhythmbox3
+flatpak install -y --noninteractive  flathub org.ardour.Ardour
+flatpak install -y --noninteractive  flathub org.atheme.audacious
+flatpak install -y --noninteractive  flathub org.clementine_player.Clementine
+flatpak install -y --noninteractive  flathub com.github.wwmm.easyeffects
+flatpak install -y --noninteractive  flathub com.googleplaymusicdesktopplayer.GPMDP
+flatpak install -y --noninteractive  flathub org.gnome.Rhythmbox3
