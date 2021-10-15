6b0be11553f107e3cd5d91a30d55aaeb3882640d
Edward B <32598399+RPTST@users.noreply.github.com>
2021-10-15 18:27:09 -0400

    Update Standard_Apps.sh

--- scripts/flatpack/Standard_Apps.sh
+++ scripts/flatpack/Standard_Apps.sh
@@ -1,66 +1,67 @@
+#!/bin/bash
 # Standard Apps install
 #
 # Productivity
-flatpak install flathub com.abisource.AbiWord
-flatpak install flathub org.libreoffice.LibreOffice
-flatpak install flathub com.simplenote.Simplenote
+flatpak install -y --noninteractive flathub com.abisource.AbiWord
+flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice
+flatpak install -y --noninteractive flathub com.simplenote.Simplenote
 #
 # Apps
-flatpak install flathub org.filezillaproject.Filezilla
-flatpak install flathub com.poweriso.PowerISO
-flatpak install flathub org.qbittorrent.qBittorren
-flatpak install flathub com.calibre_ebook.calibre
-flatpak install flathub io.bit3.WhatsAppQT
-flatpak install flathub io.github.peazip.PeaZip
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub com.github.hluk.copyq
+flatpak install -y --noninteractive flathub org.filezillaproject.Filezilla
+flatpak install -y --noninteractive flathub com.poweriso.PowerISO
+flatpak install -y --noninteractive flathub org.qbittorrent.qBittorren
+flatpak install -y --noninteractive flathub com.calibre_ebook.calibre
+flatpak install -y --noninteractive flathub io.bit3.WhatsAppQT
+flatpak install -y --noninteractive flathub io.github.peazip.PeaZip
+flatpak install -y --noninteractive flathub com.bitwarden.desktop
+flatpak install -y --noninteractive flathub com.github.hluk.copyq
 #
 # Browser
-flatpak install flathub org.gnome.Epiphany
-flatpak install flathub io.gitlab.librewolf-community
-flatpak install flathub com.github.Eloston.UngoogledChromium
+flatpak install -y --noninteractive flathub org.gnome.Epiphany
+flatpak install -y --noninteractive flathub io.gitlab.librewolf-community
+flatpak install -y --noninteractive flathub com.github.Eloston.UngoogledChromium
 #
 #graphics & videos
-flatpak install flathub io.mpv.Mpv
-flatpak install flathub org.videolan.VLC
-flatpak install flathub com.rawtherapee.RawTherapee
-flatpak install flathub com.hamrick.VueScan
-flatpak install flathub io.github.JaGoLi.ytdl_gui
-flatpak install flathub org.tuxpaint.Tuxpaint
-flatpak install flathub org.flameshot.Flameshot
+flatpak install -y --noninteractive flathub io.mpv.Mpv
+flatpak install -y --noninteractive flathub org.videolan.VLC
+flatpak install -y --noninteractive flathub com.rawtherapee.RawTherapee
+flatpak install -y --noninteractive flathub com.hamrick.VueScan
+flatpak install -y --noninteractive flathub io.github.JaGoLi.ytdl_gui
+flatpak install -y --noninteractive flathub org.tuxpaint.Tuxpaint
+flatpak install -y --noninteractive flathub org.flameshot.Flameshot
 #
 # Developer Tools
-flatpak install flathub com.vscodium.codium
-flatpak install flathub io.github.shiftey.Desktop
-flatpak install flathub com.notepadqq.Notepadqq
-flatpak install flathub com.sublimetext.three
+flatpak install -y --noninteractive flathub com.vscodium.codium
+flatpak install -y --noninteractive flathub io.github.shiftey.Desktop
+flatpak install -y --noninteractive flathub com.notepadqq.Notepadqq
+flatpak install -y --noninteractive flathub com.sublimetext.three
 #
 # Install Chat
-flatpak install flathub com.slack.Slack
-flatpak install flathub im.pidgin.Pidgin
-flatpak install flathub io.github.Hexchat
-flatpak install flathub com.microsoft.Teams
+flatpak install -y --noninteractive flathub com.slack.Slack
+flatpak install -y --noninteractive flathub im.pidgin.Pidgin
+flatpak install -y --noninteractive flathub io.github.Hexchat
+flatpak install -y --noninteractive flathub com.microsoft.Teams
 #
 # Install video conference
-flatpak install flathub com.skype.Client
-flatpak install flathub us.zoom.Zoom
-flatpak install flathub org.jitsi.jitsi-meet
-flatpak install flathub com.bluejeans.BlueJeans
+flatpak install -y --noninteractive flathub com.skype.Client
+flatpak install -y --noninteractive flathub us.zoom.Zoom
+flatpak install -y --noninteractive flathub org.jitsi.jitsi-meet
+flatpak install -y --noninteractive flathub com.bluejeans.BlueJeans
 #
 # Install voice conference
-flatpak install flathub info.mumble.Mumble
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.teamspeak.TeamSpeak
+flatpak install -y --noninteractive flathub info.mumble.Mumble
+flatpak install -y --noninteractive flathub com.discordapp.Discord
+flatpak install -y --noninteractive flathub com.teamspeak.TeamSpeak
 #
 # Install mail
-flatpak install flathub ch.protonmail.protonmail-bridge
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub ch.protonmail.protonmail-import-export-app
+flatpak install -y --noninteractive flathub ch.protonmail.protonmail-bridge
+flatpak install -y --noninteractive flathub org.mozilla.Thunderbird
+flatpak install -y --noninteractive flathub ch.protonmail.protonmail-import-export-app
 #
 # Install audio
-flatpak install flathub org.ardour.Ardour
-flatpak install flathub org.atheme.audacious
-flatpak install flathub org.clementine_player.Clementine
-flatpak install flathub com.github.wwmm.easyeffects
-flatpak install flathub com.googleplaymusicdesktopplayer.GPMDP
-flatpak install flathub org.gnome.Rhythmbox3
+flatpak install -y --noninteractive flathub org.ardour.Ardour
+flatpak install -y --noninteractive flathub org.atheme.audacious
+flatpak install -y --noninteractive flathub org.clementine_player.Clementine
+flatpak install -y --noninteractive flathub com.github.wwmm.easyeffects
+flatpak install -y --noninteractive flathub com.googleplaymusicdesktopplayer.GPMDP
+flatpak install -y --noninteractive flathub org.gnome.Rhythmbox3
